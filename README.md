6638e7af0af34afccf5aa208d99d175417f354b7
George White <stonehippo@gmail.com>
2017-07-11 08:25:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CandleWeenie
+
+ CandleWeenie is a little LED pumpkin light. It's got a photoresistor so that it can figure out when it's dark enough to turn on its cluster of candle flicker LEDs.
--- src/CandleWeenie.ino
+++ src/CandleWeenie.ino
@@ -1,14 +1,14 @@
 /*
  CandleWeenie is a little LED pumpkin light. It's got a photoresistor so that it can figure out
  when it's dark enough to turn on its cluster of candle flicker LEDs.
- 
+
  This is, of course, overkill. The circuit could be built without a microcontroller at all.
  But what's the point of that? ;-)
- 
+
  Also, this could be done with less code. I added several methods to make the code more readable,
- but really, but it easily be optimized. Given how little it does, there's not much point.
- 
- CandleWeenie is based on a ATTiny85, though this could should work fine with just about any
+ but really, but it could easily be optimized. Given how little it does, there's not much point.
+
+ CandleWeenie is based on a ATTiny85, though this code should work fine with just about any
  microcotroller that works with the Arduino IDE. For example, porting over to an Arduino Uno
  doesn't really require any changes at all (though you'd probably want to switch over to hardware
  serial, and assign a different pin to the LED control).
@@ -48,13 +48,13 @@ boolean getLedState() {
 
 void enableLeds() {
     if (!getLedState()) {
-        setLedState(HIGH, true, "LEDs enabled");        
+        setLedState(HIGH, true, "LEDs enabled");
     }
 }
 
 void disableLeds() {
     if (getLedState()) {
-        setLedState(LOW, false, "LEDs disabled");        
+        setLedState(LOW, false, "LEDs disabled");
     }
 }
 
